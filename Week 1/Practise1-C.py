b6cc1cad0e556443b2cc315f50c625e08afdadd9
AbuSheikh <44287016+AbuSheikh@users.noreply.github.com>
2018-10-20 19:51:44 +0300

    Add files via upload

--- Week 1/Practise1-C.py
+++ Week 1/Practise1-C.py
@@ -1,16 +1,41 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[6]:
 
 
 x= int (input ("If you want to translate for celsius please enter 1 and If you want to translate for fahrenheit please enter 0 : "))
+
 if x==0 : 
+
     celsius = float (input ("Enter the  temperatures degree in celsius "))
+
     fahrenheit = (celsius * 1.8) + 32
+
     print(f"{celsius} degree Celsius is equal to {fahrenheit} degree Fahrenheit")
-else :
+
+elif x==1 :
+
     fahrenheit = float (input ("Enter the  temperatures degree in fahrenheit "))
+
     celsius = (fahrenheit - 32) / 1.8
+
     print(f"{fahrenheit} degree fahrenheit is equal to {celsius } degree celsius")
+else :
+    x= int (input(" please enter valid number "))
+    if x==0 : 
+
+        celsius = float (input ("Enter the  temperatures degree in celsius "))
+
+        fahrenheit = (celsius * 1.8) + 32
+
+        print(f"{celsius} degree Celsius is equal to {fahrenheit} degree Fahrenheit")
+
+    else :
+
+        fahrenheit = float (input ("Enter the  temperatures degree in fahrenheit "))
+
+        celsius = (fahrenheit - 32) / 1.8
+
+        print(f"{fahrenheit} degree fahrenheit is equal to {celsius } degree celsius")
 
